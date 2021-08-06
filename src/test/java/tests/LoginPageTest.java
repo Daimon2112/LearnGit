ea81966b27bb96abe17abac99f3ff649b7d30e61
Daimon21 <testzadan@gmail.com>
2021-08-06 16:28:49 +0300

    add allure anotation

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 
 
+import io.qameta.allure.*;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.LoginPage;
@@ -16,6 +17,13 @@ public class LoginPageTest extends BaseTest{
         loginPage = new LoginPage(driver);
     }
 
+    @Description("Learning ALLURE")
+    @TmsLink("number in jiras")
+    @Issue("link to bug or task")
+    @Feature("groupirovanie test cases")
+    @Story("groupirovanie po storiyam")
+    @Owner("autor testa")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void checkLogin(){
         loginPage.login("Daimon2112", "21121992Dimon");
